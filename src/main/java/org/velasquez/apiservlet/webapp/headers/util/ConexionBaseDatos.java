bfab3cc1787f5ebfc9c2f32614416dee597d3d28
Rodvels <rodrigovelas2@gmail.com>
2025-01-14 15:26:13 -0500

    feat(util): Mejore la documentación de la conexion a la base de datos

--- src/main/java/org/velasquez/apiservlet/webapp/headers/util/ConexionBaseDatos.java
+++ src/main/java/org/velasquez/apiservlet/webapp/headers/util/ConexionBaseDatos.java
@@ -3,26 +3,16 @@ package org.velasquez.apiservlet.webapp.headers.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-/**
- *
- * Clase donde se maneja las configuraciones de las base de datos
- * @author Velasquez Quiroz Rodrigo Andres
- * @version 1
- * @date 1/08/2024
- * @time 22:25
- */
 
+
+//  Clase donde se configura la conexion a la base de datos
 public class ConexionBaseDatos {
 
     private static String url = "jdbc:mysql://localhost:3306/ServletJDBC?serverTimezone=America/Santiago";
     private static String username = "root";
     private static String password = "root";
 
-    /**
-     * Método que obtiene la conexión de la base de datos y regresa la petición
-     * @return Regresa la cadena de conexión
-     * @throws SQLException
-     */
+    //Obtiene la conexion de la base de datos
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, username, password);
     }
